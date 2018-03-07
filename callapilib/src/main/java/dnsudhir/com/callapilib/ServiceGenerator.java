dba4ab121ac82275770a83750348743fe11c808a
NERVENERD\NerveNerd <dnsudhir@gmail.com>
2018-03-08 01:22:56 +0530

    Lambda calls removed

--- callapilib/src/main/java/dnsudhir/com/callapilib/ServiceGenerator.java
+++ callapilib/src/main/java/dnsudhir/com/callapilib/ServiceGenerator.java
@@ -11,6 +11,7 @@ public class ServiceGenerator {
   private static String ROOT_URL = "";
   private static Retrofit.Builder builder;
   private static Retrofit retrofit;
+  private static int timeout = 60;
 
   private static HttpLoggingInterceptor loggingInterceptor =
       new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY);
@@ -20,10 +21,29 @@ public class ServiceGenerator {
     ROOT_URL = root_url;
     builder =
         new Retrofit.Builder().baseUrl(ROOT_URL).addConverterFactory(GsonConverterFactory.create());
-    retrofit = builder.client(httpClient.readTimeout(60, TimeUnit.SECONDS)
-        .connectTimeout(60, TimeUnit.SECONDS)
+    retrofit = builder.client(httpClient.readTimeout(ServiceGenerator.timeout, TimeUnit.SECONDS)
+        .connectTimeout(ServiceGenerator.timeout, TimeUnit.SECONDS)
         .addInterceptor(loggingInterceptor)
         .build()).build();
+  }
+
+  public static void build(String root_url, int timeout, boolean allowIntercept) {
+    ROOT_URL = root_url;
+    ServiceGenerator.timeout = timeout;
+    if (allowIntercept) {
+      builder = new Retrofit.Builder().baseUrl(ROOT_URL)
+          .addConverterFactory(GsonConverterFactory.create());
+      retrofit = builder.client(httpClient.readTimeout(timeout, TimeUnit.SECONDS)
+          .connectTimeout(timeout, TimeUnit.SECONDS)
+          .addInterceptor(loggingInterceptor)
+          .build()).build();
+    } else {
+      builder = new Retrofit.Builder().baseUrl(ROOT_URL)
+          .addConverterFactory(GsonConverterFactory.create());
+      retrofit = builder.client(httpClient.readTimeout(timeout, TimeUnit.SECONDS)
+          .connectTimeout(timeout, TimeUnit.SECONDS)
+          .build()).build();
+    }
   }
 
   public static <S> S createService(Class<S> serviceClass) {
